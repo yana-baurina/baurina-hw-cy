72cf54c34fd090e9341f840466c9ad80812d0b14
“yana-baurina” <baurina1@gmail.com>
2023-05-18 16:17:27 +0300

    hm done

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
-  pageLoadTimeout: 15000,  
+  pageLoadTimeout: 50000,  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
